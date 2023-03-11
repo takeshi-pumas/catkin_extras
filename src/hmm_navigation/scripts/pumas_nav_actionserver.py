5677df989e0b928dfb4e8f9896ac7c6ae2c8ad30
Ruben Sandoval <rsandovr@gmail.com>
2023-03-10 19:51:48 -0600

    changes

--- src/hmm_navigation/scripts/pumas_nav_actionserver.py
+++ src/hmm_navigation/scripts/pumas_nav_actionserver.py
@@ -18,27 +18,33 @@ import yaml
 
 
 ##################################################
-#Por probar
+# Por probar
 class nav_status():
     def __init__(self):
         self.suscriber = rospy.Subscriber(
             '/navigation/status',
-           GoalStatus, self._callback)
+            GoalStatus, self._callback)
         self._status = None
+
     def _callback(self, goal_status):
         self._status = goal_status.status
+
     def get_status(self):
         return self._status
 
 ##################################################
-#YAML reader
-def read_yaml(known_locations_file = '/known_locations.yaml'):
+# YAML reader
+
+
+def read_yaml(known_locations_file='/known_locations.yaml'):
     rospack = rospkg.RosPack()
     file_path = rospack.get_path('config_files') + known_locations_file
 
     with open(file_path, 'r') as file:
         content = yaml.safe_load(file)
     return content
+
+
 def match_location(location):
     content = read_yaml()
     try:
@@ -46,113 +52,111 @@ def match_location(location):
     except:
         return False, 'No location found'
 ##################################################
-#Navigation functions 
-def pose2feedback(pose_robot,quat_robot,timeleft,euclD):
+# Navigation functions
+
+
+def pose2feedback(pose_robot, quat_robot, timeleft, euclD):
     feed = NavigateActionFeedback()
-    feed.feedback.x_robot   = pose_robot[0]
-    feed.feedback.y_robot   = pose_robot[1]
-    _,_,yaw = tf.transformations.euler_from_quaternion(quat_robot) 
+    feed.feedback.x_robot = pose_robot[0]
+    feed.feedback.y_robot = pose_robot[1]
+    _, _, yaw = tf.transformations.euler_from_quaternion(quat_robot)
     feed.feedback.yaw_robot = yaw
-    feed.feedback.timeleft  = timeleft
+    feed.feedback.timeleft = timeleft
     feed.feedback.euclD = euclD
     return feed
 
 
 def goal_police(goal):
-    #goal corrector
+    # goal corrector
     goal_corrected = goal
-
 
     return goal_corrected
 
 ##################################################
+
+
 class pumas_navServer():
     def __init__(self):
         self.pumas_nav_server = actionlib.SimpleActionServer(
-            "navigate",NavigateAction, 
-            execute_cb=self.execute_cb, 
+            "navigate", NavigateAction,
+            execute_cb=self.execute_cb,
             auto_start=False)
         self.pumas_nav_server.start()
-    
+
     def execute_cb(self, goal):
         #########
-        #goal police
+        # goal police
         #goal_corrected = goal_police(goal)
         #########
-        success = True
+        success = False
 
-        #Matching known location if given
-        x,y,yaw = goal.x, goal.y, goal.yaw
+        # Matching known location if given
+        x, y, yaw = goal.x, goal.y, goal.yaw
         known_loc = goal.known_location.casefold()
         if known_loc != 'None':
-            succ,loc = match_location(known_loc)
+            succ, loc = match_location(known_loc)
             if succ:
                 XYT = loc[:3]
-                x, y, yaw = XYT[0]['x'], XYT[1]['y'], XYT[2]['theta'] #it could change
+                # it could change
+                x, y, yaw = XYT[0]['x'], XYT[1]['y'], XYT[2]['theta']
                 goal.x, goal.y, goal.yaw = x, y, yaw
-        
-        # print (goal)
 
-
-        head.set_joint_values(head_pose = [0.0,-0.5])
-        #Fill result message (could be modified)
+        head.set_joint_values(head_pose=[0.0, -0.5])
+        # Fill result message (could be modified)
         result = NavigateActionResult()
         rate = rospy.Rate(10)
         timeout = rospy.Time.now().to_sec() + goal.timeout
         rot = tf.transformations.quaternion_from_euler(0, 0, yaw)
         goal_pose = PoseStamped()
-        goal_pose.pose.position = Point(x,y,0)
+        goal_pose.pose.position = Point(x, y, 0)
         goal_pose.pose.orientation = Quaternion(*rot)
         goal_nav_publish.publish(goal_pose)
 
-        # result.result.success = 2
-        i = 0
-
-        #Feedback publisher and timeout checker
+        # Feedback publisher and timeout checker
         while timeout >= rospy.Time.now().to_sec() and not success:
-
-            #Goal distance and orientation calculation
+            # Goal distance and orientation calculation
             c_pose, q_rob = tf_man.getTF(target_frame='base_link')
-            _,_,rob_yaw = tf.transformations.euler_from_quaternion(q_rob)
+            _, _, rob_yaw = tf.transformations.euler_from_quaternion(q_rob)
             rob_yaw = rob_yaw % (2 * np.pi)
-            euclD = np.linalg.norm(np.asarray((x,y)) - c_pose[:2])
+            euclD = np.linalg.norm(np.asarray((x, y)) - c_pose[:2])
             anglD = yaw - rob_yaw
 
-            timeleft = timeout - rospy.Time.now().to_sec()  
-            print (timeleft,'timeleft')   
-            feed = pose2feedback(c_pose, q_rob, timeleft, euclD)
-            self.pumas_nav_server.publish_feedback(feed.feedback)
+            timeleft = timeout - rospy.Time.now().to_sec()
+            if timeleft > 0:
+                print(timeleft, 'timeleft')
+                feed = pose2feedback(c_pose, q_rob, timeleft, euclD)
+                self.pumas_nav_server.publish_feedback(feed.feedback)
 
-            state = NS.get_status()
-            
-            success = state == 3 and euclD < 0.05 and anglD < 0.1 
-            #if state == 3:
-            #    success= True
-                # self.pumas_nav_server.set_succeeded()
+            # state = NS.get_status()
 
-        state = NS.get_status()
+            # success = state == 3 and euclD < 0.05 and anglD < 0.3
+            success = euclD < 0.05 and anglD < 0.3
+
+        # state = NS.get_status()
+        head.set_joint_values(head_pose = [0.0, 0.0])
+        rospy.sleep(0.8)
         pub_stop.publish()
-        head.set_joint_values(head_pose=[0.0,0.0])
         if not success:
             self.pumas_nav_server.set_aborted()
         else:
             self.pumas_nav_server.set_succeeded()
 
 
-if __name__=="__main__":
-    global goal_nav_publish , pub_stop, NS, tf_man
+if __name__ == "__main__":
+    global goal_nav_publish, pub_stop, tf_man
     rospy.init_node('pumas_navigation_actionlib_server')
-    
+
     #pub = rospy.Publisher('/hsrb/command_velocity', Twist, queue_size=1)
-    #pub2 = rospy.Publisher('/aa/Markov_NXT/', PointStamped, queue_size=1)  
+    #pub2 = rospy.Publisher('/aa/Markov_NXT/', PointStamped, queue_size=1)
     #pub3= rospy.Publisher('aa/Markov_route',MarkerArray,queue_size=1)
     #pub_goal= rospy.Publisher('/clicked_point',PointStamped,queue_size=1)
 
     head = GAZE()
-    goal_nav_publish = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=1)
+    goal_nav_publish = rospy.Publisher(
+        '/move_base_simple/goal', PoseStamped, queue_size=1)
     tf_man = TF_MANAGER()
     pub_stop = rospy.Publisher('/navigation/stop', Empty, queue_size=10)
-    print ('pumas nav action server available')
-    NS = nav_status()
+    print('pumas nav action server available')
+    # NS = nav_status()
     s = pumas_navServer()
     rospy.spin()
--- src/smaches/utils/src/utils/nav_utils.py
+++ src/smaches/utils/src/utils/nav_utils.py
@@ -1,35 +1,40 @@
 # -*- coding: utf-8 -*-
 from utils.misc_utils import *
 
+
 class OMNIBASE():
     def __init__(self):
-        self._base_vel_pub = rospy.Publisher('/hsrb/command_velocity', Twist, queue_size=10)
+        self._base_vel_pub = rospy.Publisher(
+            '/hsrb/command_velocity', Twist, queue_size=10)
         self.velX = 0
         self.velY = 0
         self.velT = 0
         self.timeout = 0.5
         self.MAX_VEL = 0.03
         self.MAX_VEL_THETA = 0.5
-        self.navclient = actionlib.SimpleActionClient('/navigate', NavigateAction)   ### PUMAS NAV ACTION LIB
+        self.navclient = actionlib.SimpleActionClient(
+            '/navigate', NavigateAction)  # PUMAS NAV ACTION LIB
         self._tf_man = TF_MANAGER()
+
     def _move_base_vel(self):
-            twist = Twist()
-            twist.linear.x = self.velX
-            twist.linear.y = self.velY
-            twist.angular.z = self.velT
-            self._base_vel_pub.publish(twist)
+        twist = Twist()
+        twist.linear.x = self.velX
+        twist.linear.y = self.velY
+        twist.angular.z = self.velT
+        self._base_vel_pub.publish(twist)
 
     def _move_base_time(self):
-            start_time = rospy.Time.now().to_sec()
-            while rospy.Time.now().to_sec() - start_time < self.timeout:  
-                self._move_base_vel()
+        start_time = rospy.Time.now().to_sec()
+        while rospy.Time.now().to_sec() - start_time < self.timeout:
+            self._move_base_vel()
     # Move base using relative movements
-    def tiny_move(self, velX = 0, velY = 0, velT = 0, std_time = 0.5, MAX_VEL = 0.03, MAX_VEL_THETA = 0.5):
+
+    def tiny_move(self, velX=0, velY=0, velT=0, std_time=0.5, MAX_VEL=0.03, MAX_VEL_THETA=0.5):
         self.MAX_VEL = MAX_VEL
         self.MAX_VEL_THETA = MAX_VEL_THETA
         self.timeout = std_time
-        if abs(velX) > MAX_VEL: 
-            self.velX =  MAX_VEL * (velX / abs(velX))
+        if abs(velX) > MAX_VEL:
+            self.velX = MAX_VEL * (velX / abs(velX))
         else:
             self.velX = velX
         if abs(velY) > MAX_VEL:
@@ -42,12 +47,14 @@ class OMNIBASE():
             self.velT = velT
         self._move_base_time()
     # Move base using navigation (absolute movement)
-    def move_d_to(self, target_distance = 0.5, target_link='None'):
-    ###Face towards Targetlink and get target distance close
+
+    def move_d_to(self, target_distance=0.5, target_link='None'):
+        # Face towards Targetlink and get target distance close
         if target_link != 'None':
-            target, _ = self._tf_man.getTF(target_frame = target_link)
-            robot, _ = self._tf_man.getTF(target_frame = 'base_link')
-            pose,_ = self._tf_man.getTF(target_frame= target_link, ref_frame='base_link')
+            target, _ = self._tf_man.getTF(target_frame=target_link)
+            robot, _ = self._tf_man.getTF(target_frame='base_link')
+            pose, _ = self._tf_man.getTF(
+                target_frame=target_link, ref_frame='base_link')
         else:
             return False
         delta = np.asarray(target) - np.asarray(robot)
@@ -55,35 +62,35 @@ class OMNIBASE():
         theta_goal = np.arctan2(delta[1], delta[0])
         x_goal = target[0] - target_distance * np.cos(theta_goal)
         y_goal = target[1] - target_distance * np.sin(theta_goal)
-        succ = self.move_base(goal_x = x_goal, goal_y = y_goal, goal_yaw = theta_goal)
+        succ = self.move_base(
+            goal_x=x_goal, goal_y=y_goal, goal_yaw=theta_goal)
         return True
 
+    def move_base(self, goal_x=0.0, goal_y=0.0, goal_yaw=0.0, time_out=10, known_location='None'):
+        # Create and fill Navigate Action Goal message
+        nav_goal = NavigateActionGoal()
+        nav_goal.goal.x = goal_x
+        nav_goal.goal.y = goal_y
+        nav_goal.goal.yaw = goal_yaw
+        nav_goal.goal.timeout = time_out
+        nav_goal.goal.known_location = known_location
+        print(nav_goal)
 
-    def move_base(self,goal_x=0.0,goal_y=0.0,goal_yaw=0.0,time_out=10, known_location='None'):
-    	#Create and fill Navigate Action Goal message
-    	nav_goal = NavigateActionGoal()
-    	nav_goal.goal.x = goal_x
-    	nav_goal.goal.y = goal_y
-    	nav_goal.goal.yaw = goal_yaw
-    	nav_goal.goal.timeout = time_out
-    	nav_goal.goal.known_location = known_location
-    	print (nav_goal)
+        # send message to the action server
+        self.navclient.send_goal(nav_goal.goal)
 
-    	# send message to the action server
-    	self.navclient.send_goal(nav_goal.goal)
-
-    	# wait for the action server to complete the order
-    	self.navclient.wait_for_result(timeout=rospy.Duration(time_out))
-    	# Results of navigation
-    	# PENDING         = 0
-    	# ACTIVE          = 1
-    	# PREEMPTED       = 2
-    	# SUCCEEDED       = 3
-    	# ABORTED         = 4
-    	# REJECTED        = 5
-    	# PREEMPTING      = 6
-    	# RECALLING       = 7
-    	# RECALLED        = 8
-    	# LOST            = 9
-    	action_state = self.navclient.get_state()
-    	return action_state
+        # wait for the action server to complete the order
+        self.navclient.wait_for_result(timeout=rospy.Duration(time_out))
+        # Results of navigation
+        # PENDING         = 0
+        # ACTIVE          = 1
+        # PREEMPTED       = 2
+        # SUCCEEDED       = 3
+        # ABORTED         = 4
+        # REJECTED        = 5
+        # PREEMPTING      = 6
+        # RECALLING       = 7
+        # RECALLED        = 8
+        # LOST            = 9
+        action_state = self.navclient.get_state()
+        return action_state
