3f827c037158c6f4ec9855980ffa6e00bd167651
takeshi-pumas <robottakeshi@gmail.com>
2023-03-20 19:36:25 -0600

    human detector openpose cv2

---
+++ src/human_detector/scripts/utils.py
@@ -0,0 +1,134 @@
+#! /usr/bin/env python3
+import tf
+import cv2
+import rospy  
+import tf2_ros                                    
+from human_detector.srv import Human_detector ,Human_detectorResponse 
+from cv_bridge import CvBridge
+from object_classification.srv import *
+import tf2_ros    
+from segmentation.msg import *
+
+import numpy as np
+import ros_numpy
+import os
+import matplotlib.pyplot as plt
+
+from sensor_msgs.msg import Image , LaserScan , PointCloud2
+from geometry_msgs.msg import TransformStamped, Pose
+from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
+
+#-----------------------------------------------------------------
+global tf_listener, ptcld_lis, broadcaster , bridge , net
+
+
+rospy.init_node('human_detector') 
+tfBuffer = tf2_ros.Buffer()
+tfBuffer = tf2_ros.Buffer()
+listener2 = tf2_ros.TransformListener(tfBuffer)
+listener = tf.TransformListener()
+
+broadcaster = tf2_ros.TransformBroadcaster()
+tf_static_broadcaster = tf2_ros.StaticTransformBroadcaster()
+
+protoFile = "/home/oscar/Documents/pose/body_25/pose_deploy.prototxt"
+weightsFile = "/home/oscar/Documents/pose/body_25/pose_iter_584000.caffemodel"
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+#tf_listener = tf.TransformListener()
+#broadcaster= tf.TransformBroadcaster()
+#tf_static_broadcaster= tf2_ros.StaticTransformBroadcaster()
+#pub = rospy.Publisher('/segmented_images', Image, queue_size=1)
+bridge=CvBridge()
+
+#-----------------------------------------------------------------
+def write_tf(pose, q, child_frame , parent_frame='map'):
+    t= TransformStamped()
+    t.header.stamp = rospy.Time.now()
+    #t.header.stamp = rospy.Time(0)
+    t.header.frame_id =parent_frame
+    t.child_frame_id =  child_frame
+    t.transform.translation.x = pose[0]
+    t.transform.translation.y = pose[1]
+    t.transform.translation.z = pose[2]
+    #q = tf.transformations.quaternion_from_euler(eu[0], eu[1], eu[2])
+    t.transform.rotation.x = q[0]
+    t.transform.rotation.y = q[1]
+    t.transform.rotation.z = q[2]
+    t.transform.rotation.w = q[3]
+    return t
+#-----------------------------------------------------------------
+def read_tf(t):
+    pose=np.asarray((
+        t.transform.translation.x,
+        t.transform.translation.y,
+        t.transform.translation.z
+        ))
+    quat=np.asarray((
+        t.transform.rotation.x,
+        t.transform.rotation.y,
+        t.transform.rotation.z,
+        t.transform.rotation.w
+        ))
+    
+    return pose, quat
+
+#-----------------------------------------------------------------
+
+
+def probmap_to_3d_mean(points_data,probMap, thres_prob=0.3):
+    ##Prob map to 3d point
+
+    mask=np.where(probMap>thres_prob)
+    npmask=np.asarray(mask).T
+
+    npmask.shape
+    xyz=[]
+    if len (npmask)>1:
+        for a in npmask:
+            ix,iy=a[0],a[1]
+            aux=(np.asarray((points_data['x'][ix,iy],points_data['y'][ix,iy],points_data['z'][ix,iy])))
+            #print (aux)
+            if np.isnan(aux[0]) or np.isnan(aux[1]) or np.isnan(aux[2]):
+                    'reject point'
+            else:
+                xyz.append(aux)
+
+    xyz=np.asarray(xyz)
+    #print (xyz)
+    cent=xyz.mean(axis=0)
+    return cent
+
+
+def detect_human(points_msg):
+    points_data = ros_numpy.numpify(points_msg)    
+    image_data = points_data['rgb'].view((np.uint8, 4))[..., [2, 1, 0]]   
+    image=cv2.cvtColor(image_data, cv2.COLOR_BGR2RGB)
+    #image = points_data['rgb'].view((np.uint8, 4))[..., [2, 1, 0]]
+    #rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    print (image.shape)
+    frame=image
+    inHeight = frame.shape[0]
+    inWidth = frame.shape[1]
+
+
+    # Prepare the frame to be fed to the network
+    inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight), (0, 0, 0), swapRB=False, crop=False)
+
+    # Set the prepared object as the input blob of the network
+    net.setInput(inpBlob)
+
+    output = net.forward()
+    i = 0 #Face
+    #i = 1# Neck
+    probMap = output[0, i, :, :]
+    probMap = cv2.resize(probMap, (inWidth, inHeight))
+    cent= probmap_to_3d_mean(points_data,probMap)
+    res=Human_detectorResponse()
+    res.x= cent[0]
+    res.y= cent[1]
+    res.z= cent[2]
+    print (res)
+    return res    
+
+
+    
