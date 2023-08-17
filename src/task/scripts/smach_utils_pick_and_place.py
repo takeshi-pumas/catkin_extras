e8e8cde2e883536c1ffd0c0f9c773c7b58648443
takeshi robot <takeshi@takeshi.com>
2023-08-17 15:14:51 -0600

    pick and place demo

--- src/task/scripts/smach_utils_pick_and_place.py
+++ src/task/scripts/smach_utils_pick_and_place.py
@@ -2,7 +2,7 @@
 
 import smach
 import smach_ros
-from geometry_msgs.msg import PoseStamped, Point , PointStamped , Quaternion , TransformStamped , Twist, Pose
+from geometry_msgs.msg import PoseStamped, Point, PointStamped, Quaternion, TransformStamped, Twist, Pose
 from std_srvs.srv import Trigger, TriggerResponse 
 import tf2_ros
 from tf2_sensor_msgs.tf2_sensor_msgs import do_transform_cloud
@@ -22,12 +22,20 @@ from utils.misc_utils import *
 from utils.nav_utils import *
 
 global rgbd, head, wrist, tf_man, gripper, omni_base, wrist, cabeza, voice
-global clear_octo_client, scene, goal, navclient, arm
+global goal, navclient, arm, robot, scene, clear_octo_client, calibrate_wrist
 
-rospy.init_node('smach')
+
+#wbw = moveit_commander.MoveGroupCommander('whole_body_weighted')
+rospy.init_node("SMACH_pick_place_cassette")
 
 #----------Moveit instances
-arm = moveit_commander.MoveGroupCommander('whole_body_weighted')
+moveit_commander.roscpp_initialize(sys.argv)
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()
+arm = moveit_commander.MoveGroupCommander('arm')
+
+arm.set_planning_time(10)
+arm.set_num_planning_attempts(25)
 
 #----------Class instances
 rgbd        = RGBD()
@@ -39,7 +47,11 @@ cabeza      = GAZE()
 brazo       = ARM()
 voice       = TALKER()
 
-#------------------------------------------------------
+#----------ROS services
+clear_octo_client = rospy.ServiceProxy('/clear_octomap', Empty)
+calibrate_wrist = rospy.ServiceProxy('/hsrb/wrist_wrench/readjust_offset',Empty)
+
+#----------Functions
 
 def wait_for_push_hand(time=10):
 
@@ -50,7 +62,6 @@ def wait_for_push_hand(time=10):
         torque = wrist.get_torque()
         if np.abs(torque[1]) > 1.0:
             print('Hand pused ready to start')
-            #takeshi_talk_pub.publish(string_to_Voice())
             voice.talk('Im ready to start')
             return True
             break
@@ -58,5 +69,3 @@ def wait_for_push_hand(time=10):
     if (rospy.get_time() - start_time >= time):
         print(time, 'secs have elapsed with no hand push')
         return False
-#
-#------------------------------------------------------
