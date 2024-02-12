41e2cb9c6173e2f37f5ea9d0fb5e5cf30bf89639
takeshi robot <takeshi@takeshi.com>
2024-02-12 15:10:15 -0600

    gui2: GUI cool

--- src/GUI/gui2.py
+++ src/GUI/gui2.py
@@ -1,103 +1,66 @@
-import subprocess
+import PySimpleGUI as sg
+import rospy
+import roslaunch
 import os
-import signal
-from PySide2.QtWidgets import QApplication, QWidget, QPushButton, QVBoxLayout, QLabel
 
-class ROSControlWidget(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.navigation_process = None
-        self.yolo_process = None
-        self.moveit_process = None
-        self.moveit_arm_process = None
-        self.processes = []
+# Initial setup 
+os.system("source /home/takeshi/catkin_extras/devel/setup.bash")
+os.environ['ROS_MASTER_URI'] = 'http://hsrb.local_et:11311'
+os.environ['ROS_IP'] = '169.254.2.172'
 
-        self.init_ui()
+# roslaunch setup
+uuid = roslaunch.rlutil.get_or_generate_uuid(None, False)
+roslaunch.configure_logging(uuid)
 
-    def init_ui(self):
-        layout = QVBoxLayout()
+def cleanup_nodes():
+    for node in nodes.values():
+        node.shutdown()
 
-        self.navigation_button = QPushButton("Run Navigation")
-        self.navigation_button.clicked.connect(self.handle_navigation_button)
-        layout.addWidget(self.navigation_button)
+def set_node(package, launch_file):
+    cli_args = [package, f'{launch_file}.launch']
+    roslaunch_file = roslaunch.rlutil.resolve_launch_arguments(cli_args)
+    parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
+    return parent
 
-        self.yolo_button = QPushButton("Run Object Classification")
-        self.yolo_button.clicked.connect(self.handle_yolo_button)
-        layout.addWidget(self.yolo_button)
+def manage_node(node_name, package, event):
+    if nodes.get(node_name) == None:
+        nodes[node_name] = set_node(package, node_name)
+        nodes[node_name].start()
+        window[event].update(button_color = ('white', 'red'))
+    else:
+        nodes[node_name].shutdown()
+        nodes[node_name] = None
+        window[event].update(button_color = ('white', 'green'))
 
-        self.moveit_button = QPushButton("Run Moveit")
-        self.moveit_button.clicked.connect(self.handle_moveit_button)
-        layout.addWidget(self.moveit_button)
 
-        self.moveit_arm_button = QPushButton("Run Moveit test arm")
-        self.moveit_arm_button.clicked.connect(self.handle_moveit_arm_button)
-        layout.addWidget(self.moveit_arm_button)
+layout = [
+    [sg.Text("Press Button 1 to Run Navigation, Press Again to Kill Navigation")],
+    [sg.Button("Button 1", key='-NAVIGATION-', button_color=('white', 'green'))],
+    [sg.Text("Press Button 2 to Run Object Classification, Press Again to Kill Node")],
+    [sg.Button("Button 2", key='-YOLO-', button_color=('white', 'green'))],
+    [sg.Text("Press Button 3 to Run Moveit, Press Again to Kill Node")],
+    [sg.Button("Button 3", key='-MOVEIT-', button_color=('white', 'green'))],
+    [sg.Text("Press Button 4 to Run Moveit test arm, Press Again to Kill Node")],
+    [sg.Button("Button 4", key='-MOVEIT_ARM-', button_color=('white', 'green'))],
+    [sg.Image(key='-IMAGE-')],
+]
 
-        self.status_label = QLabel("Press the buttons to run ROS nodes.")
-        layout.addWidget(self.status_label)
+window = sg.Window("ROS GUI", layout)
 
-        self.setLayout(layout)
-        self.setWindowTitle("ROS Control")
+nodes = {}#{'navigation_real':None, 'hsrb_demo_with_controller':None, 'arm_test':None}
 
-    def run_command(self, command):
-        try:
-            process = subprocess.Popen(["bash", "-c", f'source ~/.bashrc && et && {command}'], preexec_fn=os.setsid, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            return process  # Return the process object
-        except FileNotFoundError:
-            self.status_label.setText(f"Command '{command.split()[0]}' not found.")
-            return None
+while True:
+    event, _ = window.read()
+    if event == sg.WINDOW_CLOSED:
+        cleanup_nodes()
+        break
+    elif event == '-NAVIGATION-':
+        manage_node('navigation_real', 'nav_pumas', event)
 
-    def kill_process(self, process):
-        if process:
-            os.killpg(os.getpgid(process.pid), signal.SIGTERM)
-            return None
+    elif event == '-MOVEIT-':
+        manage_node('hsrb_demo_with_controller', 'hsrb_moveit_config', event)
 
-    def handle_navigation_button(self):
-        if self.navigation_process:
-            self.navigation_process = self.kill_process(self.navigation_process)
-            self.navigation_button.setText("Run Navigation")
-            self.status_label.setText("Navigation node stopped.")
-        else:
-            self.navigation_process = self.run_command('roslaunch nav_pumas navigation_real.launch')
-            self.navigation_button.setText("Stop Navigation")
-            self.status_label.setText("Navigation node running.")
-            self.processes.append(self.navigation_process)
+    elif event == '-MOVEIT_ARM-':
+        manage_node('arm_test', 'task', event)
 
-    def handle_yolo_button(self):
-        if self.yolo_process:
-            self.yolo_process = self.kill_process(self.yolo_process)
-            self.yolo_button.setText("Run Object Classification")
-            self.status_label.setText("Object Classification node stopped.")
-        else:
-            self.yolo_process = self.run_command('rosrun object_classification classification_server.py')
-            self.yolo_button.setText("Stop Object Classification")
-            self.status_label.setText("Object Classification node running.")
-            self.processes.append(self.yolo_process)
-
-    def handle_moveit_button(self):
-        if self.moveit_process:
-            self.moveit_process = self.kill_process(self.moveit_process)
-            self.moveit_button.setText("Run Moveit")
-            self.status_label.setText("Moveit node stopped.")
-        else:
-            self.moveit_process = self.run_command('roslaunch hsrb_moveit_config hsrb_demo_with_controller.launch')
-            self.moveit_button.setText("Stop Moveit")
-            self.status_label.setText("Moveit node running.")
-            self.processes.append(self.moveit_process)
-
-    def handle_moveit_arm_button(self):
-        if self.moveit_arm_process:
-            self.moveit_arm_process = self.kill_process(self.moveit_arm_process)
-            self.moveit_arm_button.setText("Run Moveit test arm")
-            self.status_label.setText("Moveit test arm node stopped.")
-        else:
-            self.moveit_arm_process = self.run_command('roslaunch task arm_test.launch')
-            self.moveit_arm_button.setText("Stop Moveit test arm")
-            self.status_label.setText("Moveit test arm node running.")
-            self.processes.append(self.moveit_arm_process)
-
-if __name__ == "__main__":
-    app = QApplication([])
-    widget = ROSControlWidget()
-    widget.show()
-    app.exec_()
+window.close()
