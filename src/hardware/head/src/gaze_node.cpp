a6ce1b01e53703860e72e133995c1dcf8d3ce02c
Ruben Sandoval <rsandovr@gmail.com>
2024-02-14 19:54:36 -0600

    delete imgs

--- src/hardware/head/src/gaze_node.cpp
+++ src/hardware/head/src/gaze_node.cpp
@@ -11,6 +11,7 @@
 class GazeController {
 public:
     GazeController() : nh_("~"), tf_listener_(tf_buffer_) {
+
         // Suscribirse al tópico "/gaze/absolute"
         sub_gaze_absolute_ = nh_.subscribe("/gaze/absolute", 1, &GazeController::absoluteGazeCallback, this);
 
@@ -27,54 +28,45 @@ public:
     void absoluteGazeCallback(const std_msgs::Float32MultiArray::ConstPtr& gaze_msg) {
         // Calcular ángulos PAN y TILT con respecto a MAP
         ROS_INFO("Map absolute gaze");
-        gazePoint(gaze_msg->data, "map");
+        std::vector<float> joint_values = gazePoint(gaze_msg->data, map);
+        pub_joint_values(joint_values);
     }
 
     void relativeGazeCallback(const std_msgs::Float32MultiArray::ConstPtr& gaze_msg) {
         // Calcular ángulos PAN y TILT con respecto a base_link
         ROS_INFO("Base link relative gaze");
-
-        geometry_msgs::PointStamped point;
-
-        point.header.frame_id = "base_link";
-        point.point.x = gaze_msg->data[0]; 
-        point.point.y = gaze_msg->data[1];
-        point.point.z = gaze_msg->data[2];
-
-        tf_buffer_.transform(point, point, "map", ros::Duration(0.3));
-        std::vector<float> target;
-        target.push_back(point.point.x);
-        target.push_back(point.point.y);
-        target.push_back(point.point.z);
-        gazePoint(target, "map");
+        // std::vector<float> joint_values = calculatePanTiltAngles(gaze_msg->data, base);
+        // pub_joint_values(joint_values);
     }
 
     void TFGazeCallback(const std_msgs::String::ConstPtr& tf_name_msg){
         ROS_INFO("Gaze to a published tf");
         geometry_msgs::TransformStamped tf_position;
-        tf_position = tf_buffer_.lookupTransform("map", tf_name_msg->data, ros::Time(0), ros::Duration(0.3));
+        tf_position = tf_buffer_.lookupTransform(map, tf_name_msg->data, ros::Time(0), ros::Duration(0.3));
         std::vector<float> target;
         target.push_back(tf_position.transform.translation.x);
         target.push_back(tf_position.transform.translation.y);
         target.push_back(tf_position.transform.translation.z);
-        gazePoint(target, "map");
+        std::vector<float> joint_values = gazePoint(target, map);
+        pub_joint_values(joint_values);
     }
 
-    void gazePoint(const std::vector<float>& target_position, const std::string& ref_frame){
+    std::vector<float> gazePoint(const std::vector<float>& target_position, const std::string& ref_frame){
         try{
             geometry_msgs::TransformStamped trans;
             geometry_msgs::TransformStamped rot;
             double yaw = 0.0;
 
-            trans = tf_buffer_.lookupTransform(ref_frame, "head_rgbd_sensor_link", ros::Time(0), ros::Duration(0.3));
+            trans = tf_buffer_.lookupTransform(ref_frame, cam, ros::Time(0), ros::Duration(0.3));
 
-            if (ref_frame == "map"){
-                rot = tf_buffer_.lookupTransform(ref_frame, "base_link", ros::Time(0), ros::Duration(0.3));
+            if (ref_frame == map){
+                rot = tf_buffer_.lookupTransform(ref_frame, base, ros::Time(0), ros::Duration(0.3));
                 tf2::Quaternion quat(rot.transform.rotation.x, rot.transform.rotation.y, rot.transform.rotation.z, rot.transform.rotation.w);
                 double roll, pitch;
                 tf2::Matrix3x3(quat).getRPY(roll, pitch, yaw);
             }
-
+            
+            // Angles calculation
             double x_rob = trans.transform.translation.x;
             double y_rob = trans.transform.translation.y;
             double z_rob = trans.transform.translation.z;
@@ -87,15 +79,42 @@ public:
             double pan = fmod((-yaw + D_th + M_PI), (2*M_PI));
             double tilt = -atan2(D_z, sqrt(D_x * D_x + D_y * D_y));
 
-            std_msgs::Float32MultiArray head_control_msg;
-            head_control_msg.data.push_back(pan);
-            head_control_msg.data.push_back(tilt);
+            // Angles correction
+            if(pan > M_PI){ 
+                pan -= 2 * M_PI;
+            } else if(pan < M_PI){
+                pan += 2* M_PI;
+            }
+            if(abs(pan) > M_PI_2){
+                ROS_WARN("Exorcist alert");
+                pan = 0;
+            }
 
-            ROS_INFO("Publishing head angles: %f, %f", pan, tilt);
-            pub_head_control_.publish(head_control_msg);
+
+            // Return values
+            std::vector<float> joint_values;
+            joint_values.push_back(pan);
+            joint_values.push_back(tilt);
+            return joint_values;
+
         } catch (tf2::TransformException& ex) {
             ROS_ERROR("Error al transformar la posicion objetivo: %s", ex.what());
         }
+    }
+
+    // std::vector<float> calculatePanTiltAngles(const std::vector<float>& target_point, const std::string& ref_frame){
+    //     std::vector<float> targetTransform = tf_buffer_.transform(target_point, cam,ros::Duration(0.3));
+    //     ROS_INFO("Vector transformed: %f", targetTransform);
+    //     return targetTransform;
+    // }
+
+    void pub_joint_values(const std::vector<float>& joint_values){
+            std_msgs::Float32MultiArray head_control_msg;
+            // head_control_msg.data.push_back(joint_values[0]);
+            // head_control_msg.data.push_back(joint_values[1]);
+            head_control_msg.data = joint_values;
+            ROS_INFO("Publishing head angles: %f, %f", joint_values[0], joint_values[1]);
+            pub_head_control_.publish(head_control_msg);
     }
 
 private:
@@ -106,6 +125,9 @@ private:
     ros::Subscriber sub_gaze_relative_;
     ros::Subscriber sub_gaze_tf_;
     ros::Publisher pub_head_control_;
+    const std::string map = "map";
+    const std::string cam = "head_rgbd_sensor_link";
+    const std::string base = "base_link";
 };
 
 int main(int argc, char** argv) {
