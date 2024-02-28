604a3e549d7fe1e4b602b23385bfebac9967a2df
tamagawa_lap <robottakeshi@gmail.com>
2024-02-28 14:50:10 -0600

    action server grasp

---
+++ src/action_server/scripts/ACT_SRVR_grasp.py
@@ -0,0 +1,21 @@
+import moveit_commander
+import rospy
+
+class GRASP_ACTION:
+    def __init__(self):
+        whole_body = moveit_commander.MoveGroupCommander("whole_body")
+        #whole_body
+
+    def base_approach(self):
+        #
+        pass
+    
+    def approach(self):
+        pass
+
+    def grasp(self):
+        pass
+    
+    def retrait(self):
+        pass
+
