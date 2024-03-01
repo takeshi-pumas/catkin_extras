1c6dd5039f3a5b1ae7d78847f9204de7b715194c
Ruben Sandoval <rsandovr@gmail.com>
2024-03-01 09:03:36 -0600

    action server grasp need to be test

--- src/action_server/scripts/ACT_SRVR_grasp.py
+++ src/action_server/scripts/ACT_SRVR_grasp.py
@@ -1,28 +1,50 @@
 import rospy
 import sys
+import tf2_ros
+import tf2_geometry_msgs
+import numpy as np
 import smach
 from smach_ros import ActionServerWrapper
 import moveit_commander
-from geometry_msgs.msg import Pose, Point, Quaternion
+from geometry_msgs.msg import Pose, Point, Quaternion, PointStamped, PoseStamped
 from shape_msgs.msg import SolidPrimitive
-from moveit_msgs.msg import CollisionObject
+from moveit_msgs.msg import CollisionObject, AttachedCollisionObject
 from action_server.msg import GraspAction
-#from actionlib import SimpleActionServer
-#from your_package_name.msg import FollowAction, FollowGoal, FollowResult, FollowFeedback
+
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
+
+from utils.grasp_utils import *
 
 class GraspingStateMachine:
     def __init__(self):
+        # Import gripper controller
+        self.gripper = GRIPPER()
+
+        # Inicializar tf2_ros
+        self.tf2_buffer = tf2_ros.Buffer()
+        self.listener = tf2_ros.TransformListener(self.tf2_buffer)
+
         # Inicializar MoveIt
         moveit_commander.roscpp_initialize(sys.argv)
         self.robot = moveit_commander.RobotCommander()
         self.scene = moveit_commander.PlanningSceneInterface()
-        self.group_name = "whole_body"
-        self.group = moveit_commander.MoveGroupCommander(self.group_name)
+        self.whole_body = moveit_commander.MoveGroupCommander("whole_body")
+        #self.arm = moveit_commander.MoveGroupCommander("arm")
+        self.grasp_approach = "frontal" #above
+
+        # Moveit setup
+        #self.arm.allow_replanning(True)
+        self.whole_body.allow_replanning(True)
+        self.whole_body.set_num_planning_attempts(5)
+        self.whole_body.set_planning_time(7.0)
+        #self.arm.set_num_planning_attempts(5)
+        #self.arm.set_planning_time(7.0)
+        self.whole_body.set_workspace([-2.0, -2.0, 2.0, 2.0])
 
         # Definir la posición objetivo (cambiar según necesidad)
         self.target_pose = Pose()
         self.target_pose.position.x = 0.5
-        self.target_pose.position.y = 0.5
+        self.target_pose.position.y = 0.0
         self.target_pose.position.z = 0.5
         self.target_pose.orientation.w = 1.0
 
@@ -50,53 +72,150 @@ class GraspingStateMachine:
 
         outcome = self.sm.execute()
 
+    def calculate_frontal_approach(self, target_position = [0.0, 0.0, 0.0]):
+        object_point = PointStamped()
+        object_point.header.frame_id = "base_link"
+        object_point.point.x = target_position[0]
+        object_point.point.y = target_position[1]
+        object_point.point.z = target_position[2]
+
+        #transformar la posicion del objeto al marco de referencia de la base del robot
+        try:
+            transformed_object_point = self.tf2_buffer.transform(object_point, "base_link", timeout=rospy.Duration(1))
+        except :
+            rospy.WARN("Error al transformar la posicion del objeto al marco de referencia")
+            return None, None
+        
+        # approach_positon = np.array([transformed_object_point.point.x - 0.05,
+        #                               transformed_object_point.point.y,
+        #                               transformed_object_point.point.z])
+        approach_pose = Pose()
+        approach_pose.position.x = transformed_object_point.point.x - 0.12
+        approach_pose.position.y = transformed_object_point.point.y
+        approach_pose.position.z = transformed_object_point.point.z
+        quat = quaternion_from_euler(np.pi, -np.pi/2, 0)
+        approach_pose.orientation = Quaternion(*quat)
+        #approach_pose.orientation = Quaternion(0.0, 0.7071, 0.0, 0.7071)
+        return approach_pose
+    
+    def calculate_above_approach(self, target_position = [0.0, 0.0, 0.0]):
+        object_point = PointStamped()
+        object_point.header.frame_id = "base_link"
+        object_point.point.x = target_position[0]
+        object_point.point.y = target_position[1]
+        object_point.point.z = target_position[2]
+
+        #transformar la posicion del objeto al marco de referencia de la base del robot
+        try:
+            transformed_object_point = self.tf2_buffer.transform(object_point, "base_link", timeout=rospy.Duration(1))
+        except :
+            rospy.WARN("Error al transformar la posicion del objeto al marco de referencia")
+            return None
+        
+        # approach_posiiton = np.array([transformed_object_point.point.x,
+        #                               transformed_object_point.point.y,
+        #                               transformed_object_point.point.z + 0.05])
+        approach_pose = Pose()
+        approach_pose.position.x = transformed_object_point.point.x
+        approach_pose.position.y = transformed_object_point.point.y
+        approach_pose.position.z = transformed_object_point.point.z + 0.12
+
+        quat = quaternion_from_euler(np.pi, 0, 0)
+        approach_pose.orientation = Quaternion(*quat)
+        return approach_pose
+
     def approach(self, userdata):
-        pose_goal = self.target_pose
-        approach_pose = pose_goal
-        approach_pose.position.z += 0.1  # 10 cm por encima de la pieza
-        self.move_to_pose(approach_pose)
+        #Add primitive objets to planning scene
+        pos = [self.target_pose.position.x,
+                self.target_pose.position.y,
+                self.target_pose.position.z]
+        self.add_collision_object(position = pos,dimensions = [0.05, 0.05, 0.05])
+
+        self.gripper.open()
+        #pose_goal = self.target_pose
+        #approach_pose = pose_goal
+        #approach_pose.position.z += 0.1  # 10 cm por encima de la pieza
+        pose_goal = [self.target_pose.position.x, 
+                     self.target_pose.position.y,
+                     self.target_pose.position.z]
+        if self.grasp_approach == "frontal":
+            self.target_pose = self.calculate_frontal_approach(target_position=pose_goal)
+            print(self.target_pose)
+        elif self.grasp_approach == "above":
+            self.target_pose = self.calculate_above_approach(target_position=pose_goal)
+            print(self.target_pose)
+        
+        self.move_to_pose(self.whole_body, self.target_pose)
         return 'success'
 
     def grasp(self, userdata):
+        
+        #Plan to grasp object
         pose_goal = self.target_pose
         grasp_pose = pose_goal
-        grasp_pose.position.z -= 0.05  # Descender hacia la pieza
-        self.move_to_pose(grasp_pose)
-        # Agregar la lógica para cerrar la pinza aquí
-        self.add_collision_object(0.1, 0.1, 0.1)  # Tamaño aproximado de la pieza
-        rospy.sleep(1)
+        if self.grasp_approach == "frontal":
+            grasp_pose.position.x += 0.05   # Adelantarse a la pieza
+        elif self.grasp_approach == "above":
+            grasp_pose.position.z -= 0.05  # Descender hacia la pieza
+        self.move_to_pose(self.whole_body, grasp_pose)
+
+        #gripper.close(0.04)
+        # rospy.sleep(1)
+        self.gripper.close(0.3)
+        self.attach_object()
         return 'success'
 
     def retreat(self, userdata):
         pose_goal = self.target_pose
-        self.move_to_pose(pose_goal)  # Retirarse a una posición segura
+        pose_goal.position.x -= 0.07
+        pose_goal.position.z += 0.07
+        self.move_to_pose(self.whole_body, pose_goal)  # Retirarse a una posición segura
         return 'success'
 
-    def move_to_pose(self, pose_goal):
-        self.group.set_pose_target(pose_goal)
-        plan = self.group.go(wait=True)
+    def move_to_pose(self, group, pose_goal):
+        group.set_pose_target(pose_goal)
+        plan = group.go(wait=True)
         rospy.sleep(1)
 
-    def add_collision_object(self, x, y, z):
-        collision_object = CollisionObject()
-        collision_object.id = "objeto"
-        collision_object.header.frame_id = self.group.get_planning_frame()
-        primitive = SolidPrimitive()
-        primitive.type = SolidPrimitive.BOX
-        primitive.dimensions = [x, y, z]
-        pose = Pose()
-        pose.position.x = x
-        pose.position.y = y
-        pose.position.z = z
-        collision_object.primitives.append(primitive)
-        collision_object.primitive_poses.append(pose)
-        self.scene.add_object(collision_object)
+    def add_collision_object(self, position = [0, 0, 0], dimensions = [0.1 ,0.1, 0.1]):
+        # collision_object = CollisionObject()
+        # collision_object.id = "objeto"
+        # collision_object.header.frame_id = "map" #self.group.get_planning_frame()
+        # primitive = SolidPrimitive()
+        # primitive.type = SolidPrimitive.BOX
+        # primitive.dimensions = dimensions
+        # pose = Pose()
+        # pose.position.x = position[0]
+        # pose.position.y = position[1]
+        # pose.position.z = position[2]
+        # collision_object.primitives.append(primitive)
+        # collision_object.primitive_poses.append(pose)
+        # self.scene.add_object(collision_object)
+        object_pose = PoseStamped()
+        object_pose.header.frame_id = "map"
+        object_pose.pose = self.target_pose
+        self.scene.add_box("objeto", object_pose, size = (dimensions[0], dimensions[1], dimensions[2]))
+
+    def attach_object(self):
+
+        grasping_group = "arm"
+        touch_links = self.robot.get_link_names(group=grasping_group)
+        eef_link = self.whole_body.get_end_effector_link()
+
+        self.scene.attach_box(eef_link, "objeto", touch_links=touch_links)
+        # attached_object = AttachedCollisionObject()
+        # attached_object.object.id = "objeto"
+        # attached_object.link_name = "hand_palm_link"
+
+        # attached_object.object.operation = attached_object.object.ADD
+
+        # self.scene.attach_object(attached_object)
 
     def execute_cb(self, goal):
         rospy.loginfo('Received action goal: %s', goal)
         self.sm.userdata.goal = goal
         self.wrapper.server.set_succeeded()
-        #outcome = self.sm.execute()
+        outcome = self.sm.execute()
         # result = FollowResult()
         # result.success = True if outcome == 'success' else False
         # self.server.set_succeeded(result)
@@ -104,4 +223,4 @@ class GraspingStateMachine:
 if __name__ == '__main__':
     rospy.init_node('grasping_demo')
     grasping_sm = GraspingStateMachine()
-    rospy.spin()
+    #rospy.spin()
