891ebe0fad9decc970fe6040af38e5b632254c95
Joel David Gago Garcia <jdgago9811@gmail.com>
2025-03-31 12:05:32 -0600

    hard coding

--- src/object_classification/src/classification_server_receptionist.py
+++ src/object_classification/src/classification_server_receptionist.py
@@ -25,7 +25,7 @@ clip_model, clip_preprocess = clip.load("ViT-B/32", device=device)
 rospy.loginfo("CLIP model loaded successfully.")
 
 # Set base directory
-BASE_DIR = os.path.expanduser("~/GroundingDINO")
+BASE_DIR = os.path.expanduser("~/Repositories/GroundingDINO")
 CONFIG_PATH = os.path.join(BASE_DIR, "groundingdino/config/GroundingDINO_SwinT_OGC.py")
 WEIGHTS_PATH = os.path.join(BASE_DIR, "weights", "groundingdino_swint_ogc.pth")
 
