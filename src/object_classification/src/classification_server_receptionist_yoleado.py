74708690a7f6bbcac5999249db935cc38658645a
Angel Hernandez <hernandezabarcaangel@gmailcom>
2025-06-20 12:36:35 -0600

    Agrega classification_server_receptionist_yoleado.py

--- src/object_classification/src/classification_server_receptionist_yoleado.py
+++ src/object_classification/src/classification_server_receptionist_yoleado.py
@@ -5,29 +5,35 @@ import torch
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
 from std_msgs.msg import String
-from object_classification.srv import Classify_dino_receptionist, Classify_dino_receptionistResponse
+import rospkg
+import os
+#from object_classification.srv import Classify_dino_receptionist, Classify_dino_receptionistResponse
 from object_classification.srv import Classify, ClassifyResponse
 from ultralytics import YOLO
 import numpy as np
 
 bridge = CvBridge()
 
-# Carga modelo YOLOv8 (puede ser yolov8n.pt o tu modelo entrenado)
+# Carga modelo YOLOv8 
 device = "cuda" if torch.cuda.is_available() else "cpu"
-model_path = "/home/angel/ANGEL/Angel_YOLO/yolo_11.pt"
-model = YOLO(model_path)
+rospack= rospkg.RosPack()
+file_path = rospack.get_path('object_classification')
+ycb_yolo_path=file_path+'/src/weights/yolo_11.pt'
+
+model=YOLO(ycb_yolo_path)
+#model_path = "/home/angel/ANGEL/Angel_YOLO/yolo_11.pt"
+#model = YOLO(model_path)
 rospy.loginfo(f"YOLOv8 model loaded on {device}")
 
-CONFIDENCE_THRESHOLD = 0.3  # Umbral para filtrar detecciones
+CONFIDENCE_THRESHOLD = 0.3  
 
 def handle_detection(req):
-    # Convertir imagen ROS a OpenCV BGR
+    # imagen ROS a OpenCV BGR
     image = bridge.imgmsg_to_cv2(req.image, desired_encoding="bgr8")
 
-    # Ejecutar inferencia
     results = model(image)
 
-    # Extraer detecciones: boxes, confidences
+
     boxes = []
     confidences = []
     for r in results:
@@ -43,15 +49,15 @@ def handle_detection(req):
         rospy.logwarn("No detections with confidence above threshold.")
         return ClassifyResponse(result=String(data="not found"))
 
-    # Calcular el centro x de cada caja para ordenar
+    # centro x de cada caja para ordenar
     centers_x = [ (b[0]+b[2])/2 for b in boxes ]
     sorted_indices = np.argsort(centers_x)
 
-    # Ordenar cajas y confiabilidades
+    
     boxes = [boxes[i] for i in sorted_indices]
     confidences = [confidences[i] for i in sorted_indices]
 
-    # Asignar posición a cada caja (left, center, right)
+
     labeled_positions = []
     for i in range(len(boxes)):
         if i == 0:
@@ -62,7 +68,7 @@ def handle_detection(req):
             pos = "center"
         labeled_positions.append(pos)
 
-    # Seleccionar la caja con mayor confianza
+    
     max_conf_idx = np.argmax(confidences)
     best_position = labeled_positions[max_conf_idx]
 
